8c80a7540cb71ea2451a15d8acbf309d7bec241b
Ian Reynolds <idreyn@gmail.com>
2023-12-11 21:02:04 -0800

    modern stuff

--- gain.py
+++ gain.py
@@ -1,6 +1,7 @@
 import ad5252
 import click
 
+
 @click.command()
 @click.option("--write", default=None, help="Write to one or both channels")
 @click.option("--read", is_flag=True, help="Read from both channels")
@@ -8,17 +9,14 @@ def main(write, read):
     ad = ad5252.AD5252()
     if write is not None:
         if "," in write:
-            res = [
-                float(part.strip())
-                for part in write.split(",")
-            ]
+            res = [float(part.strip()) for part in write.split(",")]
         else:
             res = 2 * [float(write.strip())]
         [ad.write(value, channel) for channel, value in enumerate(res)]
     if read is not None:
-        res = [ad.read(channel) for channel in [0,1]]
-        print " ".join([str(val) for val in res])
-                  
+        res = [ad.read(channel) for channel in [0, 1]]
+        print(" ".join([str(val) for val in res]))
+
 
 if __name__ == "__main__":
     main()
